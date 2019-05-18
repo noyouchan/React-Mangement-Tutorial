9c81917becba98ab2b66eeaea2efb20b6f8103e9
Youchan No <youchan9133@gmail.com>
2019-05-19 02:15:19 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-// Component를 불러오는 방법
 import React, { Component } from 'react';
 import './App.css';
-// 해당 component 폴더에 있는 Customer.js를 불러와야 한다.
-import Customer from './components/Customer'
+import Customer from './components/Customer';
 
-// 여러 사용자 정보를 위해 배열로 선언
+// material-ui에서 table library 가져오기
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+// CSS
+import { withStyles } from '@material-ui/core/styles';
+
+// 1080px 보다 작아질 때는 가로 스크롤바가 생기도록 만듬
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
 const customers = [{
   'id': 1,
   'image': 'https://placeimg.com/64/64/1',
@@ -33,53 +53,29 @@ const customers = [{
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      // 1개 이상의 데이터가 추가되면 div로 감싸줘야 함
-      <div>
-        {
-          // map 함수를 이용해서 반복적인 내용을 반복문으로 해결
-          customers.map(c => {
-            return <Customer
-              key={c.id}  // map은 key(props)값으로 각 데이터를 구분
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          })
-        }
-
-        {/* 너무 반복적이서 소스가 길어짐
-        <Customer
-          // Props를 이용하여 정보 전달
-          id={customers[0].id}
-          image={customers[0].image}
-          name={customers[0].name}
-          birthday={customers[0].birthday}
-          gender={customers[0].gender}
-          job={customers[0].job}
-        />
-        <Customer
-          id={customers[1].id}
-          image={customers[1].image}
-          name={customers[1].name}
-          birthday={customers[1].birthday}
-          gender={customers[1].gender}
-          job={customers[1].job}
-        />
-        <Customer
-          id={customers[2].id}
-          image={customers[2].image}
-          name={customers[2].name}
-          birthday={customers[2].birthday}
-          gender={customers[2].gender}
-          job={customers[2].job}
-        />   */}
-      </div>
+      /* 화면 크기에 따라 내용이 유동적으로 조절 */
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { customers.map(c => { return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+// 적용한 상태로 내보내기
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,39 +1,19 @@
 import React from 'react';
+// material-ui에서 테이블을 사용
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
-// Component를 상속받아서 Component로서 사용할 수 있도록 함
 class Customer extends React.Component {
     render() {
         return (
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
-        )
-    }
-}
-
-// 계층화해서 사용자 데이터를 구분
-// 프로필 이미지, 이름 출력
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-// 나머지 데이터 출력
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
