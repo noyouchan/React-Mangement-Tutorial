34614f80e022ec76121f8a98759019aa9b6bf0b7
Youchan No <youchan9133@gmail.com>
2019-05-19 01:39:24 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+// Component를 불러오는 방법
+import React, { Component } from 'react';
 import './App.css';
+// 해당 component 폴더에 있는 Customer.js를 불러와야 한다.
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+// const는 변수 선언
+const customer = {
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}
+
+class App extends Component {
+  render() {
+    return (
+      // 다음과 같이 호출
+      <Customer
+        // Props를 이용하여 정보 전달
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
